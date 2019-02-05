0be802d46a264f2f7bd1bf28842f70f732e1bc2b
Victor Rentea <victorrentea@gmail.com>
2019-02-05 15:10:20 +0200

    rename local variable SHIFT-F6 in Idea, ALT-SHIFT-R in Eclipse

--- src/main/java/videostore/dirty/Customer.java
+++ src/main/java/videostore/dirty/Customer.java
@@ -25,31 +25,31 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
-			Rental each = (Rental) rentals.nextElement();
+			Rental rental = (Rental) rentals.nextElement();
 			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
+			switch (rental.getMovie().getPriceCode()) {
 			case Movie.REGULAR:
 				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				if (rental.getDaysRented() > 2)
+					thisAmount += (rental.getDaysRented() - 2) * 1.5;
 				break;
 			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
+				thisAmount += rental.getDaysRented() * 3;
 				break;
 			case Movie.CHILDRENS:
 				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				if (rental.getDaysRented() > 3)
+					thisAmount += (rental.getDaysRented() - 3) * 1.5;
 				break;
 			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
+			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&& rental.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
+			result += "\t" + rental.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
