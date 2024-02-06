21de90317196a0ad58dcaa8bd05488bf1084f431
Victor Rentea <victorrentea@gmail.com>
2024-02-06 17:22:26 +0200

    yip

--- src/main/java/videostore/horror/RentedMovie.java
+++ src/main/java/videostore/horror/RentedMovie.java
@@ -1,38 +1,38 @@
 package videostore.horror;
 
 public record RentedMovie(
-        Movie movie,
-        int days
+    Movie movie,
+    int days
 ) {
 
-    int calculateFrequentRenterPoints() {
-        if (movie.category() != null &&
-                (movie.category() == Category.NEW_RELEASE)
-                && days > 1) {
-            return 2;
-        }
-        return 1;
+  int calculateFrequentRenterPoints() {
+    if (movie.category() != null &&
+        (movie.category() == Category.NEW_RELEASE)
+        && days > 1) {
+      return 2;
     }
+    return 1;
+  }
 
-    public double calculateRentalPrice() {
-            double rentalPrice = 0;
-        switch (movie().category()) {
-            case REGULAR -> {
-                rentalPrice = 2;
-                if (days> 2)
-                    rentalPrice += (days - 2) * 1.5;
-                return rentalPrice;
-            }
-            case NEW_RELEASE -> {
-                return days * 3;
-            }
-            case CHILDRENS -> {
-                rentalPrice = 1.5;
-                if (days > 3)
-                    rentalPrice += (days - 3) * 1.5;
-                return rentalPrice;
-            }
-        }
-            return rentalPrice;
-        }
+  public double calculateRentalPrice() {
+    double rentalPrice;
+    switch (movie().category()) {
+      case REGULAR -> {
+        rentalPrice = 2;
+        if (days > 2)
+          rentalPrice += (days - 2) * 1.5;
+        return rentalPrice;
+      }
+      case NEW_RELEASE -> {
+        return days * 3;
+      }
+      case CHILDRENS -> {
+        rentalPrice = 1.5;
+        if (days > 3)
+          rentalPrice += (days - 3) * 1.5;
+        return rentalPrice;
+      }
+      default -> throw new IllegalStateException("Unexpected value: " + movie().category());
+    }
+  }
 }
