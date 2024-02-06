7b0b48ff4a6985bf6719a44d154b558744460981
Ari Yacobson <ari.yacobson@nice.com>
2024-02-06 16:49:11 +0200

    refactor extract business logic releated to rental to Rental class

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -19,21 +19,13 @@ class Customer {
 
 	public String statement() {
 		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + name + "\n";
-
+		int frequentRenterPoints = 0;
 		for (Rental movieRental : rentals) {
-			// determine amounts for movie line
-			int daysRented = movieRental.days();
-			double thisAmount = calculatePrice(movieRental.movie(), daysRented);
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (movieRental.movie().movieType() == MovieType.NEW_RELEASE && daysRented > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += movieRental.getFrequentPoints();
 			// show figures line for this rental
-			result += "\t" + movieRental.movie().title() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + movieRental.getMovie().title() + "\t" + movieRental.getPrice() + "\n";
+			totalAmount += movieRental.getPrice();
 		}
 		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
@@ -41,23 +33,6 @@ class Customer {
 		return result;
 	}
 
-	private static double calculatePrice(Movie movie, int daysRented) {
-		double thisAmount = 0;
-		switch (movie.movieType()) {
-			case REGULAR:
-				thisAmount += 2;
-				if (daysRented > 2)
-					thisAmount += (daysRented - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				thisAmount += daysRented * 3;
-				break;
-			case CHILDREN:
-				thisAmount += 1.5;
-				if (daysRented > 3)
-					thisAmount += (daysRented - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
-	}
+
+
 }
--- src/main/java/videostore/horror/Rental.java
+++ src/main/java/videostore/horror/Rental.java
@@ -1,4 +1,49 @@
 package videostore.horror;
 
-public record Rental(Movie movie, int days) {
+import lombok.Getter;
+
+@Getter
+public class Rental {
+
+    private final double price;
+    private final Movie movie;
+    private final int days;
+    private final int frequentPoints;
+
+    public Rental (Movie movie, int days) {
+        this.movie = movie;
+        this.days = days;
+        this.price = calculatePrice(movie, days);
+        this.frequentPoints = calculateFrequentRenterPoints(movie, days);
+    }
+
+    static double calculatePrice(Movie movie, int daysRented) {
+        double thisAmount = 0;
+        switch (movie.movieType()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case CHILDREN:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    private static int calculateFrequentRenterPoints(Movie movie, int daysRented) {
+        // add frequent renter points
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if (movie.movieType() == MovieType.NEW_RELEASE && daysRented > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
